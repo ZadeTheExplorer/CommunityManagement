f7f690889678e21e16c10414b54a941796505b54
Zad3TheExplorer <hatrisy@gmail.com>
2020-05-24 11:24:39 +0700

    add merger sort

--- src/Main/Sorting.java
+++ src/Main/Sorting.java
@@ -6,9 +6,84 @@ import java.util.Arrays;
 public class Sorting {
 
     public Sorting() {};
+
+    private static void merge(String [] arr, int l, int m, int r)
+    {
+        // Find sizes of two subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        /* Create temp arrays */
+        String [] L = new String [n1];
+        String [] R = new String [n2];
+
+        /*Copy data to temp arrays*/
+        for (int i=0; i<n1; ++i)
+            L[i] = arr[l + i];
+        for (int j=0; j<n2; ++j)
+            R[j] = arr[m + 1+ j];
+
+        /* Merge the temp arrays */
+
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+
+        // Initial index of merged subarry array
+        int k = l;
+        while (i < n1 && j < n2)
+        {
+            if (R[j].compareTo(L[j]) >= 0 )
+            {
+                arr[k] = L[i];
+                i++;
+            }
+            else
+            {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        /* Copy remaining elements of L[] if any */
+        while (i < n1)
+        {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        /* Copy remaining elements of R[] if any */
+        while (j < n2)
+        {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    // Main function that sorts arr[l..r] using
+    // merge()
+    public static String[] mergeSort(String[] arr, int minIndex, int maxIndex)
+    {
+        if (minIndex < maxIndex)
+        {
+            // Find the middle point
+            int m = (minIndex+maxIndex)/2;
+
+            // Sort first and second halves
+            mergeSort(arr, minIndex, m);
+            mergeSort(arr , m+1, maxIndex);
+
+            // Merge the sorted halves
+            merge(arr, minIndex, m, maxIndex);
+        }
+        return arr;
+    }
+
     //TODO: Implement Merge Sort
     public static void Sort(String[] array){
-
+        mergeSort(array, 0, array.length -1);
     }
 
     public static void Sort(int[] array){
